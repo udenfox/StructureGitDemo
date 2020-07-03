c61dc1ff4bfa9f12e84b04cbdfa0787c65261c2e
d.shevtsov <d.shevtsov@keepsolid.com>
2020-07-03 12:49:41 +0300

    Students array added

--- app/src/main/java/com/keepsolid/structuregitdemo/MainActivity.java
+++ app/src/main/java/com/keepsolid/structuregitdemo/MainActivity.java
@@ -4,11 +4,20 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> students = new ArrayList<>();
+        students.add("Student1");
+        students.add("Student2");
+        students.add("Student3");
+        students.add("Student4");
+
     }
 }
